9e4556f05727b498172c28313f622c03019d9ad2
yavaka <yavakasolutions@gmail.com>
2021-07-04 11:07:40 +0100

    Restructured client-side folder architecture and added bootstrap

---
+++ Valkar/client-app/src/app/layout/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
