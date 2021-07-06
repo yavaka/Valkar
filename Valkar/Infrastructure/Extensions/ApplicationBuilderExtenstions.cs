5284b8f79f6efc25d78ce8f8e389de399db47bce
yavaka <yavakasolutions@gmail.com>
2021-07-06 13:29:24 +0100

    Added extension automatic migration of the database

---
+++ Valkar/Infrastructure/Extensions/ApplicationBuilderExtenstions.cs
@@ -0,0 +1,18 @@
+﻿namespace Infrastructure.Extensions
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class ApplicationBuilderExtenstions
+    {
+        public static void ApplyMigrations(this IApplicationBuilder app)
+        {
+            using var services = app.ApplicationServices.CreateScope();
+
+            var dbContext = services.ServiceProvider.GetService<ValkarDbContext>();
+
+            dbContext.Database.Migrate();
+        }
+    }
+}
