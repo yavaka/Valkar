04054638a80697fda1cadea3be72e85d3b3c21d0
yavaka <yavakasolutions@gmail.com>
2021-07-19 14:06:16 +0100

    Added enum with driving licence categories.

---
+++ Valkar/Infrastructure/Common/Enums/DrivingLicenceCategories.cs
@@ -0,0 +1,21 @@
+﻿namespace Infrastructure.Common.Enums
+{
+    public enum DrivingLicenceCategories
+    {
+        AM = 0,
+        A1 = 1,
+        A2 = 2,
+        A = 3,
+        B1 = 4,
+        B = 5,
+        C1 = 6,
+        C = 7,
+        D1 = 8,
+        D = 9,
+        BE = 10,
+        C1E = 11,
+        CE = 12,
+        D1E = 13,
+        DE = 14
+    }
+}
