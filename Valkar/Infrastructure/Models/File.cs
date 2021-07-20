66e064a08e6df84033c99d939fb11b818b20fcb9
yavaka <yavakasolutions@gmail.com>
2021-07-20 18:00:02 +0100

    Added File model.

---
+++ Valkar/Infrastructure/Models/File.cs
@@ -0,0 +1,15 @@
+﻿namespace Infrastructure.Models
+{
+    using System;
+    
+    public class File
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string FileType { get; set; }
+        public string Extension { get; set; }
+        public Guid UploadedById { get; set; }
+        public Driver UploadedBy { get; set; }
+        public DateTime UploadedOn { get; set; }
+    }
+}
